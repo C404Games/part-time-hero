204908b7eb97a7adf16f77a49eb4a436fc29f6fd
apozag <32688343+apozag@users.noreply.github.com>
2021-03-09 00:57:58 +0100

    Estructura IA

---
+++ Assets/Scripts/AI/AIAgent.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum State
+{
+    WAITING,
+    GRABBING_RESOURCE,
+    ELABORATING,
+    DELIVERING
+}
+
+public class AIAgent : MonoBehaviour
+{
+
+    bool busy = false;
+
+    State state = State.WAITING;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case State.WAITING:
+                break;
+            case State.GRABBING_RESOURCE:
+                break;
+            case State.ELABORATING:
+                break;
+            case State.DELIVERING:
+                break;
+        }
+    }
+
+    public bool isBusy()
+    {
+        return busy;
+    }
+}
---
+++ Assets/Scripts/AI/BehaviourNode.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum NodeType
+{
+    AND,
+    OR,
+    ALWAYS_TRUE,
+    ALWAYS_FALSE
+}
+
+public class BehaviourNode
+{
+    List<BehaviourNode> children;
+
+    public delegate bool performAction(AIAgent agent);
+
+    public performAction action;
+
+    NodeType type;
+
+    public BehaviourNode(NodeType type, performAction action, List<BehaviourNode> children)
+    {
+        this.type = type;
+        this.action = action;
+        this.children = children;
+    }
+
+    public bool doTree(AIAgent agent)
+    {
+        foreach(BehaviourNode node in children)
+        {
+            bool success = node.doTree(agent);
+            if (type == NodeType.AND && !success)
+                return false;
+            else if (type == NodeType.OR && success)
+                return true;
+        }
+        return action(agent);
+    }
+
+}
