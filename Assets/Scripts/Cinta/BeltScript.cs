64bdd7fd07b19daee5f38ec510728a3ecddaf5d6
celestial97 <elcelestial97@gmail.com>
2021-03-25 01:45:42 +0100

    Añadir cinta transportadora

---
+++ Assets/Scripts/Cinta/BeltScript.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeltScript : MonoBehaviour
+{
+    public float speed = 1;
+    public float rotate = 0;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    float incremental = 0;
+    private void FixedUpdate()
+    {
+        //Guardamos valores originales.
+        Vector3 pos = rb.position;
+        Quaternion rot = rb.rotation;
+
+        //Actualizamos posicion de los objetos movibles.
+        Vector3 fatherRotation = transform.parent.rotation.eulerAngles;
+        rb.position += new Vector3(Mathf.Cos(Mathf.PI / 180 * fatherRotation.y), 0, -Mathf.Sin(Mathf.PI / 180 * fatherRotation.y)) * speed * Time.fixedDeltaTime;
+        rb.MovePosition(pos);
+
+
+        //NO FUNCIONAL, SE DEJA COMENTADO PARA NO PERDER EL CÓDIGO POR SI LUEGO SE REANUDA.
+        //Actualizamos la rotacion de los objetos movibles.
+        //rBody.rotation = Quaternion.Euler(0, rotate, 0);
+        //rb.MoveRotation(rot);
+    }
+}
