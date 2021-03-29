bf654c25f793105105b488cd0ff0d76c953e9612
apozag <32688343+apozag@users.noreply.github.com>
2021-03-29 20:24:38 +0200

    Modelos nuevos + IA Monstruos

--- Assets/Scripts/Input/PlayerMovement.cs
+++ Assets/Scripts/Input/PlayerMovement.cs
@@ -10,16 +10,20 @@ public class PlayerMovement : MonoBehaviour
     public float rotSpeed = 10.0f;
     public Animator animator;
 
+    public GameObject sword;
+
     Controls controls;
 
     Vector3 velocity;
     
 
     Rigidbody rb;
-    NavMeshAgent agent;
+    NavMeshAgent nvAgent;
 
     Vector3 destination;
 
+    MonsterController monsterInReach;
+    bool attackBusy = false;
 
 
     private void Awake()
@@ -30,17 +34,18 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
+        nvAgent = GetComponent<NavMeshAgent>();
         rb = GetComponent<Rigidbody>();
         velocity = new Vector2(0, 0);
     }
 
     private void OnEnable()
     {
-        controls.Gameplay.Move.performed += c => OnMove(c);
-        controls.Gameplay.Move.started += c => OnMove(c);
-        controls.Gameplay.Move.canceled += c => OnMove(c);
+        controls.Gameplay.Movement.performed += c => OnMove(c);
+        controls.Gameplay.Movement.started += c => OnMove(c);
+        controls.Gameplay.Movement.canceled += c => OnMove(c);
         controls.Gameplay.MouseClick.performed += c => onClick(c);
+        controls.Gameplay.Attack.performed += c => onAttack(c);
         controls.Gameplay.Enable();
     }
 
@@ -54,15 +59,31 @@ public class PlayerMovement : MonoBehaviour
         //Vector2 dx = velocity * Time.deltaTime;
         //transform.position += new Vector3(dx.y, 0.0f, -dx.x);
         rb.velocity = velocity;
-        if (velocity.sqrMagnitude != 0 && (!agent.hasPath || agent.velocity.sqrMagnitude == 0f))
+        if (velocity.sqrMagnitude != 0 && (!nvAgent.hasPath || nvAgent.velocity.sqrMagnitude == 0f))
             transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(velocity), Time.deltaTime * rotSpeed);
-        if ((!agent.hasPath || agent.velocity.sqrMagnitude == 0f) && rb.velocity == new Vector3(0, 0, 0))
+        if ((!nvAgent.hasPath || nvAgent.velocity.sqrMagnitude == 0f) && rb.velocity == new Vector3(0, 0, 0))
         {
             animator.SetBool("isRunning", false);
         }
         else
         {
             animator.SetBool("isRunning", true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (monsterInReach == null && other.gameObject.tag.Equals("Monster"))
+        {
+            monsterInReach = other.GetComponent<MonsterController>();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Monster") && other.GetComponent<MonsterController>() == monsterInReach)
+        {
+            monsterInReach = null;
         }
     }
 
@@ -78,7 +99,7 @@ public class PlayerMovement : MonoBehaviour
 
         velocity = (forward * input.y + right * input.x) * speed;        
 
-        agent.isStopped = true;
+        nvAgent.isStopped = true;
     }
 
     public void onClick(InputAction.CallbackContext context)
@@ -90,10 +111,18 @@ public class PlayerMovement : MonoBehaviour
             if (hit.collider.tag.Equals("Floor"))
             {
                 destination = hit.point;
-                agent.SetDestination(destination);
-                agent.isStopped = false;
+                nvAgent.SetDestination(destination);
+                nvAgent.isStopped = false;
             }
         }
+    }
+
+    public void onAttack(InputAction.CallbackContext context)
+    {
+
+            animator.SetTrigger("Attack");
+            monsterInReach.takeHealth(1);
+            attackBusy = true;
     }
 
 }
---
+++ Assets/Scripts/Monsters/MonsterController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+using System.Linq;
+
+public enum MonsterState
+{
+    ATTACKING,
+    WALKING, 
+    WAITING
+}
+
+
+public class MonsterController : MonoBehaviour
+{
+
+    MonsterState state;
+
+    NavMeshAgent nvAgent;
+
+    StationInstance target;
+
+    double attackTime = 2.0;
+    double lastTime = 0.0;
+
+    int health;
+
+    Animator animator;
+
+    //ProductManager productManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = MonsterState.WAITING;
+        nvAgent = GetComponent<NavMeshAgent>();
+        health = 5;
+        animator = GetComponent<Animator>();
+        //productManager = FindObjectOfType<ProductManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case MonsterState.WAITING:
+                IEnumerable<StationInstance> stations = FindObjectsOfType<StationInstance>().Where(s => s.getHealth() > 0);
+                int randIdx = Random.Range(0, stations.Count() - 1);
+                target = stations.ElementAt(randIdx);
+                nvAgent.SetDestination(target.gameObject.transform.position);
+                nvAgent.isStopped = false;
+                state = MonsterState.WALKING;
+                animator.SetBool("Walking", true);
+                break;
+            case MonsterState.WALKING:
+                if(target == null)
+                {
+                    animator.SetBool("Walking", false);
+                    state = MonsterState.WAITING;
+                    break;
+                }
+                if(!nvAgent.hasPath)
+                {
+                    animator.SetBool("Walking", false);
+                    lastTime = Time.time;
+                    nvAgent.isStopped = true;
+                    state = MonsterState.ATTACKING;
+                }
+                break;
+            case MonsterState.ATTACKING:
+                if(target == null || target.getHealth() <= 0)
+                {
+                    state = MonsterState.WAITING;
+                    target = null;
+                }
+
+                if(Time.time - lastTime >= attackTime)
+                {
+                    animator.SetTrigger("Attack");
+                    lastTime = Time.time;
+                    target.takeHealth(1);
+                }
+                break;
+        }
+    }
+
+    public void takeHealth(int h)
+    {
+        health -= h;
+        if(health <= 0)
+        {
+            animator.SetBool("Dead", true);
+            //Destroy(this.gameObject);
+        }
+        else
+        {
+            animator.SetTrigger("Hit");
+        }
+    }
+}
