c39ffe204a5cdd619521194e4f5d7cef52e400e2
celestial97 <elcelestial97@gmail.com>
2021-04-21 20:52:50 +0200

    Arreglos pequeños fallos manu y añadido contador gemas

---
+++ Assets/Scripts/MainMenu/UpdateCoins.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateCoins : MonoBehaviour
+{
+    private GodScript god;
+    // Start is called before the first frame update
+    void Start()
+    {
+        god = GameObject.FindGameObjectWithTag("God").GetComponent<GodScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Text>().text = god.getCoins().ToString();
+    }
+}
