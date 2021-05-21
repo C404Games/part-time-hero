8a1257a1c29863c1290ac0e4e8df7347d005d0e3
celestial97 <elcelestial97@gmail.com>
2021-05-21 11:34:21 +0200

    Minimals errors

---
+++ Assets/playersCounter.cs
@@ -0,0 +1,18 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playersCounter : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(null != PhotonNetwork.CurrentRoom)
+        {
+            GetComponent<Text>().text = PhotonNetwork.CurrentRoom.PlayerCount + "/" + PhotonNetwork.CurrentRoom.MaxPlayers;
+        }
+    }
+}
