422e18a3c7a35241c917c92988479ba85ab9fb94
carlosrodrigorivero <48279283+carlosrodrigorivero@users.noreply.github.com>
2021-05-21 20:24:56 +0200

    menu behaviour update

--- Assets/Scripts/UI/MenuBehaviour.cs
+++ Assets/Scripts/UI/MenuBehaviour.cs
@@ -479,21 +479,21 @@ public class MenuBehaviour : MonoBehaviour
                     victoryOrDefeatText.text = "VICTORIA";
                     VictoryOrDefeatBackText.text = "VICTORIA";
                     victoryOrDefeatText.color = Color.red;
-                    VictoryOrDefeatBackText.color = Color.red;
+                    VictoryOrDefeatBackText.color = Color.black;
                 }
                 if (language == "English")
                 {
                     victoryOrDefeatText.text = "VICTORY";
                     VictoryOrDefeatBackText.text = "VICTORY";
                     victoryOrDefeatText.color = Color.red;
-                    VictoryOrDefeatBackText.color = Color.red;
+                    VictoryOrDefeatBackText.color = Color.black;
                 }
                 if (language == "Simplified Chinese")
                 {
                     victoryOrDefeatText.text = "勝利";
                     VictoryOrDefeatBackText.text = "勝利";
                     victoryOrDefeatText.color = Color.red;
-                    VictoryOrDefeatBackText.color = Color.red;
+                    VictoryOrDefeatBackText.color = Color.black;
                 }
             } else
             {
@@ -502,21 +502,21 @@ public class MenuBehaviour : MonoBehaviour
                     victoryOrDefeatText.text = "DERROTA";
                     VictoryOrDefeatBackText.text = "DERROTA";
                     victoryOrDefeatText.color = Color.green;
-                    VictoryOrDefeatBackText.color = Color.green;
+                    VictoryOrDefeatBackText.color = Color.black;
                 }
                 if (language == "English")
                 {
                     victoryOrDefeatText.text = "DEFEAT";
                     VictoryOrDefeatBackText.text = "DEFEAT";
                     victoryOrDefeatText.color = Color.green;
-                    VictoryOrDefeatBackText.color = Color.green;
+                    VictoryOrDefeatBackText.color = Color.black;
                 }
                 if (language == "Simplified Chinese")
                 {
                     victoryOrDefeatText.text = "打敗";
                     VictoryOrDefeatBackText.text = "打敗";
                     victoryOrDefeatText.color = Color.green;
-                    VictoryOrDefeatBackText.color = Color.green;
+                    VictoryOrDefeatBackText.color = Color.black;
                 }
             }
 
